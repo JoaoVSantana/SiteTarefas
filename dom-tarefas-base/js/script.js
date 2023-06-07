05debeae2bff69d2bc70f3e9bfeab77c8b82d82a
JoaoVSantana <joao10victorsantana@gmail.com>
2023-06-06 21:54:53 -0300

    Primeira Versão

---
+++ dom-tarefas-base/js/script.js
@@ -0,0 +1,59 @@
+//selecionando eventos
+let campoTarefa = document.querySelector('#campo-tarefa');
+let botaoAdicionar = document.querySelector('#botao-adicionar');
+let listaTarefas = document.querySelector('#lista-tarefas');
+
+var botao = document.getElementById("botao-adicionar");
+// botao.onclick = function() {
+//     alert('Clicado');
+// }
+
+//adicionando o Evento do clique no botão Adicionar
+botaoAdicionar.addEventListener('click', adicionarTarefa);
+
+//Adicionando a tarefa com a tecla enter
+campoTarefa.addEventListener("keyup", function(enter)   {
+    if(enter.key === "Enter") {
+        adicionarTarefa();
+    }
+})
+
+//Criando a adicionar elementos
+function adicionarTarefa(){
+    //pegar o valor do campo de texto
+    let nomeTarefa = campoTarefa.value;
+
+    //criando um elemento li
+    let novaTarefa = document.createElement('li');
+    
+    novaTarefa.innerText = nomeTarefa;
+
+    //adicionar o item da lista ao final da lista
+    listaTarefas.appendChild(novaTarefa);
+
+    //Quando o item da lista é clicado adicionamos a classe "concluida"
+    novaTarefa.addEventListener('click' , function() {
+        novaTarefa.classList.toggle("concluida");
+        listaTarefas.removeChild(novaTarefa);
+
+    })
+
+    //Removendo elementos
+    //Criar botão excluir
+    let botaoExcluir = document.createElement("button");
+
+    //Definir o texto do botão excluir
+    botaoExcluir.innerText = "Remover";
+    
+    //Adicionar o botão excluir
+    novaTarefa.appendChild(botaoExcluir);
+    
+    //Limpar o campo de texto
+    campoTarefa.value = "";
+
+    //Adicionar classe botão excluir
+    botaoExcluir.className = "botao-excluir";
+
+    //Adicionar classe 
+};
+
