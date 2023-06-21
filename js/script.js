0152432095f6d444cde727bddde50de5edc3ff54
JoaoVSantana <joao10victorsantana@gmail.com>
2023-06-20 21:54:33 -0300

    aa

--- js/script.js
+++ js/script.js
@@ -56,4 +56,17 @@ function adicionarTarefa(){
 
     //Adicionar classe 
 };
+ 
+let botaoToggle = document.getElementById('toggle');
 
+document.body.className = localStorage.getItem('modo') || 'modo-claro'; 
+
+botaoToggle.addEventListener('click', function(){
+    if(document.body.className === 'modo-claro'){
+        document.body.className = 'modo-escuro'
+    } else {
+        document.body.className = 'modo-claro'
+    }
+
+    localStorage.setItem('modo' , document.body.className);
+})
