9d672296e7a85ce97d1b6cf81c4ac5661ce3d079
JoaoVSantana <joao10victorsantana@gmail.com>
2023-06-27 21:08:45 -0300

    27/06

--- js/script.js
+++ js/script.js
@@ -4,38 +4,38 @@ let botaoAdicionar = document.querySelector('#botao-adicionar');
 let listaTarefas = document.querySelector('#lista-tarefas');
 
 var botao = document.getElementById("botao-adicionar");
-// botao.onclick = function() {
-//     alert('Clicado');
-// }
-
-//adicionando o Evento do clique no botão Adicionar
-botaoAdicionar.addEventListener('click', adicionarTarefa);
+// //adicionando o Evento do clique no botão Adicionar
+// botaoAdicionar.addEventListener('click', adicionarTarefa);
 
 //Adicionando a tarefa com a tecla enter
-campoTarefa.addEventListener("keyup", function(enter)   {
-    if(enter.key === "Enter") {
-        adicionarTarefa();
+campoTarefa.addEventListener("keyup", function (enter) {
+    if (enter.key === "Enter") {
+        let nomeItem = campoTarefa.value;
+        if (nomeItem == "") {
+            nomeItem = "Vazio";
+        }
+        itensSalvos.push(nomeItem);
+        localStorage.setItem('itens', JSON.stringify(itensSalvos));
+        adicionarTarefa(nomeItem);
     }
 })
 
 //Criando a adicionar elementos
-function adicionarTarefa(){
-    //pegar o valor do campo de texto
-    let nomeTarefa = campoTarefa.value;
+function adicionarTarefa(nomeTarefa) {
+
+
 
     //criando um elemento li
     let novaTarefa = document.createElement('li');
-    
+
     novaTarefa.innerText = nomeTarefa;
 
     //adicionar o item da lista ao final da lista
     listaTarefas.appendChild(novaTarefa);
 
     //Quando o item da lista é clicado adicionamos a classe "concluida"
-    novaTarefa.addEventListener('click' , function() {
+    novaTarefa.addEventListener('click', function () {
         novaTarefa.classList.toggle("concluida");
-        listaTarefas.removeChild(novaTarefa);
-
     })
 
     //Removendo elementos
@@ -44,29 +44,56 @@ function adicionarTarefa(){
 
     //Definir o texto do botão excluir
     botaoExcluir.innerText = "Remover";
-    
+
     //Adicionar o botão excluir
     novaTarefa.appendChild(botaoExcluir);
-    
+
     //Limpar o campo de texto
     campoTarefa.value = "";
 
     //Adicionar classe botão excluir
     botaoExcluir.className = "botao-excluir";
 
-    //Adicionar classe 
+    botaoExcluir.addEventListener('click', function () {
+        listaTarefas.removeChild(novaTarefa);
+        itensSalvos.splice(itensSalvos.indexOf(nomeTarefa), 1);
+        localStorage.setItem('itens', JSON.stringify(itensSalvos));
+    });
 };
- 
+
 let botaoToggle = document.getElementById('toggle');
 
-document.body.className = localStorage.getItem('modo') || 'modo-claro'; 
+document.body.className = localStorage.getItem('modo') || 'modo-claro';
 
-botaoToggle.addEventListener('click', function(){
-    if(document.body.className === 'modo-claro'){
+botaoToggle.addEventListener('click', function () {
+    if (document.body.className === 'modo-claro') {
         document.body.className = 'modo-escuro'
     } else {
         document.body.className = 'modo-claro'
     }
 
-    localStorage.setItem('modo' , document.body.className);
+    localStorage.setItem('modo', document.body.className);
 })
+
+let itensSalvos = JSON.parse(localStorage.getItem('itens')) || [];
+itensSalvos.forEach(adicionarTarefa);
+
+botaoAdicionar.addEventListener('click', function () {
+    let nomeItem = campoTarefa.value;
+
+    if (nomeItem == "") {
+        nomeItem = "Vazio";
+
+    }
+    adicionarTarefa(nomeItem);
+    itensSalvos.push(nomeItem);
+    localStorage.setItem('itens', JSON.stringify(itensSalvos));
+    campoTarefa.value = '';
+})
+
+function adicionarItemNaLista(nomeItem) {
+    let itemLista = document.createElement('li');
+    novaTarefa.innerText = nomeItem;
+    listaCompras.appendChild(novaTarefa);
+
+}
